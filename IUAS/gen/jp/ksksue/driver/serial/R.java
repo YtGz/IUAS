50aa1cf318539f79783bd023628dec2a4d662376
Gogeta05 <martin.agreiter@student.uibk.ac.at>
2015-04-15 17:31:48 +0200

    add intent & color blob detection

--- IUAS/gen/jp/ksksue/driver/serial/R.java
+++ IUAS/gen/jp/ksksue/driver/serial/R.java
@@ -7,14 +7,28 @@
 package jp.ksksue.driver.serial;
 
 public final class R {
+	public static final class attr {
+		public static final int camera_id = 0x7f010001;
+		public static final int show_fps = 0x7f010000;
+	}
 	public static final class drawable {
-		public static final int ic_launcher = 0x7f020033;
+		public static final int ic_launcher = 0x7f020037;
+	}
+	public static final class id {
+		public static final int any = 0x7f0a0009;
+		public static final int back = 0x7f0a000a;
+		public static final int front = 0x7f0a000b;
 	}
 	public static final class layout {
 		public static final int main = 0x7f030018;
 	}
 	public static final class string {
-		public static final int app_name = 0x7f060001;
-		public static final int hello = 0x7f060000;
+		public static final int app_name = 0x7f0b0011;
+		public static final int hello = 0x7f0b0010;
+	}
+	public static final class styleable {
+		public static final int[] CameraBridgeViewBase = { 0x7f010000, 0x7f010001 };
+		public static final int CameraBridgeViewBase_camera_id = 1;
+		public static final int CameraBridgeViewBase_show_fps = 0;
 	}
 }
