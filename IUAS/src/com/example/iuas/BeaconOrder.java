8a3dd1b658a853181d654b766e2063a5b31be90f
Philipp Wirtenberger <Philipp.Wirtenberger@student.uibk.ac.at>
2015-05-30 19:31:10 +0200

    add beacon order

---
+++ IUAS/src/com/example/iuas/BeaconOrder.java
@@ -0,0 +1,28 @@
+package com.example.iuas;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import com.example.iuas.BallAndBeaconDetection.BEACON;
+
+public class BeaconOrder {
+	private static final BEACON[][] beaconPairsOrdered = {{BEACON.RED_GREEN, BEACON.RED_YELLOW}, {BEACON.RED_YELLOW, BEACON.BLUE_RED}, {BEACON.BLUE_RED, BEACON.BLUE_YELLOW}, {BEACON.BLUE_YELLOW, BEACON.BLUE_GREEN}, {BEACON.BLUE_GREEN, BEACON.YELLOW_BLUE}, {BEACON.YELLOW_BLUE, BEACON.RED_BLUE}, {BEACON.RED_BLUE, BEACON.YELLOW_RED}, {BEACON.YELLOW_RED, BEACON.RED_GREEN}}; 
+	
+	//returns 2 beacons that are neighbored, ordered clockwise
+	public static BEACON[] getTwoNeighboredBeacons(ArrayList<BEACON> beacons) {
+		for(int i = 0; i < beacons.size(); i++) {
+			for(int j = i; j < beacons.size(); j++) {
+				for(int k = 0; k < beaconPairsOrdered.length; k++) {
+					if(beaconPairsOrdered[k][0].equals(beacons.get(i)) && beaconPairsOrdered[k][1].equals(beacons.get(j))) {
+						return new BEACON[] {beacons.get(i), beacons.get(j)};
+					}
+					if(beaconPairsOrdered[k][1].equals(beacons.get(i)) && beaconPairsOrdered[k][0].equals(beacons.get(j))) {
+						return new BEACON[] {beacons.get(j), beacons.get(i)};
+					}
+				}
+			}
+		}
+		return null;
+	}
+}
